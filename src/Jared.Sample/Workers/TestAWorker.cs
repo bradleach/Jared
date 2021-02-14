eeba5b3dd0abf83f5786b83be12969ee65b75736
Brad Leach <brad@reddingo.com>
2021-02-14 17:19:09 +1100

    Added configuration support

--- src/Jared.Sample/Workers/TestAWorker.cs
+++ src/Jared.Sample/Workers/TestAWorker.cs
@@ -1,4 +1,5 @@
 ﻿using Jared.Sample.Services;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Context;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace Jared.Sample.Workers
     class TestAWorker : IWorker
     {
         private readonly IExampleService exampleService;
+        private readonly IConfiguration configuration;
 
-        public TestAWorker(IExampleService exampleService)
+        public TestAWorker(IExampleService exampleService, IConfiguration configuration)
         {
             this.exampleService = exampleService;
+            this.configuration = configuration;
         }
 
         public async Task Execute()
@@ -19,6 +22,8 @@ namespace Jared.Sample.Workers
             using (LogContext.PushProperty("Worker", "TestAWorker"))
             using (var tracker = Tracker.Start())
             {
+                Log.Information(configuration["Example"]);
+                Log.Information(configuration["TestAWorker:SomeProperty"]);
                 Log.Information("Hello TestAWorker");
             }
         }
